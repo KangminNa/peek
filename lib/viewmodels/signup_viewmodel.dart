d82d42ed151422c6beccdf0b81d1fac1e517b139
나강민 <139530542+KangminNa@users.noreply.github.com>
2024-02-22 17:36:45 +0900

    Feat : 이메일 비밀번호 유효성 검사 및 error메세지 상태 변화 체크 기능 구현

--- lib/viewmodels/signup_viewmodel.dart
+++ lib/viewmodels/signup_viewmodel.dart
@@ -2,13 +2,57 @@ import 'package:get/get.dart';
 import 'package:peek/controllers/auth_controller.dart';
 import 'package:peek/models/signup_model.dart';
 
-
 class SignupViewModel extends GetxController {
   final AuthController _authController = Get.find();
+  var emailError = RxString(''); // 이메일 오류 메시지를 Observable로 관리합니다.
+  var passwordError = RxString(''); // 비밀번호 오류 메시지를 Observable로 관리합니다.
 
   void signup(String email, String name, String password, String numbering) {
-    var signupData = SignupModel(email: email, name: name, password: password, numbering: numbering);
+    // 이메일 형식 검증
+    validateEmail(email);
 
-    _authController.signup(signupData);
+    // 비밀번호 형식 검증
+    validatePassword(password);
+
+    // 오류가 없으면 회원가입 요청
+    if (emailError.isEmpty && passwordError.isEmpty) {
+      var signupData = SignupModel(
+        email: email,
+        name: name,
+        password: password,
+        numbering: numbering,
+      );
+      _authController.signup(signupData);
+    }
+  }
+
+  // 이메일 데이터 검증 함수
+  void validateEmail(String email) {
+    if (!isValidEmail(email)) {
+      emailError.value = '올바른 이메일 형식이 아닙니다.';
+    } else {
+      emailError.value = ''; // 유효한 경우 오류 메시지를 초기화합니다.
+    }
+  }
+
+// 비밀번호 데이터 검증 함수
+  void validatePassword(String password) {
+    if (!isValidPassword(password)) {
+      passwordError.value = '비밀번호는 8-20자 사이의 특수문자와 숫자를 포함해야 합니다.';
+    } else {
+      passwordError.value = ''; // 유효한 경우 오류 메시지를 초기화합니다.
+    }
+  }
+
+  // 이메일 형식 검증 함수
+  bool isValidEmail(String email) {
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
+  }
+
+  // 비밀번호 형식 검증 함수
+  bool isValidPassword(String password) {
+    return RegExp(
+            r'^(?=.*?[a-zA-Z])(?=.*?[0-9])(?=.*?[!@#$%^&*()_+}{":;\,.<>?/]).{8,20}$')
+        .hasMatch(password);
   }
 }
--- lib/views/signup_screen.dart
+++ lib/views/signup_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:peek/viewmodels/signup_viewmodel.dart';
+import 'package:peek/views/login_screen.dart';
 
 class SignupScreen extends StatelessWidget {
   final SignupViewModel _signupViewModel = Get.put(SignupViewModel());
@@ -14,56 +15,78 @@ class SignupScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Sign Up'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
+    return WillPopScope(
+      onWillPop: () async {
+        Get.off(() => LoginScreen()); // 뒤로가기를 누르면 로그인 화면으로 이동
+        return false; // WillPopScope에 false 반환하여 뒤로가기 동작을 차단
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sign Up'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Obx(
+                () => TextField(
+                  controller: _emailController,
+                  onChanged: (value) {
+                    _signupViewModel.validateEmail(value);
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    errorText: _signupViewModel.emailError.value.isNotEmpty
+                        ? _signupViewModel.emailError.value
+                        : null,
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: 'Name',
+              const SizedBox(height: 16),
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Name',
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
+              const SizedBox(height: 16),
+              Obx(
+                () => TextField(
+                  controller: _passwordController,
+                  onChanged: (value) {
+                    _signupViewModel.validatePassword(value);
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    errorText: _signupViewModel.passwordError.value.isNotEmpty
+                        ? _signupViewModel.passwordError.value
+                        : null,
+                  ),
+                  obscureText: true,
+                ),
               ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 16),
-            TextField(
-              controller: _modelNumberController,
-              decoration: const InputDecoration(
-                labelText: 'Model Number',
+              const SizedBox(height: 16),
+              TextField(
+                controller: _modelNumberController,
+                decoration: const InputDecoration(
+                  labelText: 'Model Number',
+                ),
               ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                _signupViewModel.signup(
-                  _emailController.text,
-                  _nameController.text,
-                  _passwordController.text,
-                  _modelNumberController.text,
-                );
-              },
-              child: const Text('Sign Up'),
-            ),
-          ],
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  _signupViewModel.signup(
+                    _emailController.text,
+                    _nameController.text,
+                    _passwordController.text,
+                    _modelNumberController.text,
+                  );
+                },
+                child: const Text('Sign Up'),
+              ),
+            ],
+          ),
         ),
       ),
     );
