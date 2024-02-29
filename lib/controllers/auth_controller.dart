014ab554787dc3fa393214d94b3100682d4cc973
나강민 <139530542+KangminNa@users.noreply.github.com>
2024-02-29 17:45:46 +0900

    Update auth_controller.dart
    회원가입 API 보내고 200번대 응답이 온다면 Dialog 보내는 기능 구현

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:peek/models/login_model.dart';
 import 'package:peek/models/signup_model.dart';
 import 'package:peek/models/user_model.dart';
+import 'package:peek/views/login_screen.dart';
 
 class AuthController extends GetxController {
   var user = User(token: '').obs; // 사용자 정보를 관리합니다.
@@ -28,9 +30,24 @@ class AuthController extends GetxController {
 
     final response = await http.post(url, headers: headers, body: body);
 
-    if (response.statusCode == 200) {
-      print('Signup successful');
-      // 여기에서 필요한 로직을 수행합니다. 예를 들어, 회원가입이 성공한 후의 작업을 수행할 수 있습니다.
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      // 회원가입이 성공하면 로그인 화면으로 이동
+      Get.off(LoginScreen());
+      // 회원가입 완료 알림 표시
+      Get.dialog(
+        AlertDialog(
+          title: const Text('회원가입 완료'),
+          content: const Text('회원가입이 완료되었습니다. 로그인을 해주세요!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Get.back(); // 알림 창 닫기
+              },
+              child: const Text('확인'),
+            ),
+          ],
+        ),
+      );
     } else {
       print('Failed to sign up: ${response.statusCode}');
       // 실패한 경우에 대한 처리를 수행할 수 있습니다.
